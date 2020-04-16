3ed1b60fa6f7119fca70fcab0d148ca3d6a9464d
nat-chan <ogino.natsuki.tm@alumni.tsukuba.ac.jp>
2020-04-16 18:04:12 +0900

    提供されている512pxデータセットにはgifを前処理されているものも含まれるためgifの1フレーム目を切り出すようにした

--- rescale_images.sh
+++ rescale_images.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-rescale512px() {
-    ID=$( basename "$@"  | cut -d '.' -f 1)
-    EXT=$( basename "$@" | cut -d '.' -f 2)
-    # echo $EXT
-    BUCKET=$(printf "%04d" $(( $ID % 1000 )) )
-    # avoid animated GIFs: exploded in multiple JPGs by ImageMagick, they break the 1 ID == 1 file assumption, the associated metadata like tags may be completely wrong, and as multiple JPGs they look awful
-    #Added the JPEG:SIZE hint and after 820000
-    if [[ ! -a ./512px/$BUCKET/$ID.jpg ]] && [[ $EXT -ne "gif" ||  $(identify -format %n "$@") -eq 1 ]]; then
-        convert -resize 512x512\> -extent 512x512\> -limit thread 1 -gravity center -background white "$@" ./512px/$BUCKET/$ID.jpg
-    fi
-}
-export -f rescale512px
-
 center512px() {
     ID=$( basename "$@"  | cut -d '.' -f 1)
     EXT=$( basename "$@" | cut -d '.' -f 2)
@@ -26,6 +13,12 @@ center512px() {
                 -gravity center \
                 -extent $size \
                 "$@" ./$DST/$BUCKET/$ID.jpg
+        elif [ $EXT = "gif" ]; then
+            convert \
+                -resize "$size^" \
+                -gravity center \
+                -extent $size \
+                "$@[0]" ./$DST/$BUCKET/$ID.jpg
         else
             convert \
                 -resize "$size^" \
@@ -48,7 +41,8 @@ export -f find_images
 
 for i in {0000..0999}; do
     echo -e "\033[32m$i\033[m"
-    find original/$i -type f -iname "*.jp*g" -or -iname "*.png" -or -iname "*.bmp" | parallel -j22 --progress --no-notice --joblog=../resize_log.txt center512px
+    find original/$i -type f -iname "*.jp*g" -or -iname "*.png" -or -iname "*.gif" -or -iname "*.bmp" |\
+        parallel -j22 --progress --no-notice --joblog=./resize_log.txt center512px
 done
 
 # 1
